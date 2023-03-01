193044f7471b68c8ff5889f1b3f42062fcb00f26
David Tejerina <davidtejerina2003rgmail.com>
2023-03-01 09:41:23 +0100

    Ilustracion 1 - Version inicial

--- ejerciciogit.java
+++ ejerciciogit.java
@@ -1,16 +1,11 @@
 public static boolean hyperPar(int num){
 	int resto;
-	if(num>=0){
-		while(num!=0){
-			resto=num % 10;
-			num=num/10;
-			if(resto%2!=0){
-				return false;
-			}   
-		}
-		return true;
-	}
-	else{
+	while(num!=0){
+		resto=num % 10;
+		num=num/10;
+		if(resto%2!=0){
 			return false;
-	}	
+		}   
+	}
+	return true;
 }
